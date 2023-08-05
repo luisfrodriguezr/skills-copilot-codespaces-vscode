5b1d5e342c7ce8f98c8d4320b21a48021091a2ac
Luis Rodriguez <101071780+pmentor@users.noreply.github.com>
2023-08-05 21:40:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#333";
+    skillsBtn.style.backgroundColor = "#111";
+}
